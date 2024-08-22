92cda67dc8b6906eeca43a55f966bd36fb52f687
Jo <johj703@nate.com>
2024-08-22 21:34:41 +0900

    PokemonCard 컴포넌트 작성

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+function PokemonCard({ pokemon, onAdd, onRemove, isSelected }) {
+  return (
+    <Card>
+      <img src={""} alt={""} />
+      <p>{pokemon.korean_name}</p>
+      {isSelected ? (
+        <Button onClick={() => {}}>삭제</Button>
+      ) : (
+        <Button onClick={() => {}}>추가</Button>
+      )}
+    </Card>
+  );
+}
+
+export default PokemonCard;
