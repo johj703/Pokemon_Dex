37c7994c12d1dc4759e8748862612599c6bca0ee
Jo <johj703@nate.com>
2024-08-22 21:33:44 +0900

    PokemonList 컴포넌트 작성

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,16 @@
+const PokemonList = ({ pokemonList, onAddPokemon }) => {
+  return (
+    <ListContainer>
+      {pokemonList.map((pokemon) => (
+        <PokemonCard
+          key={pokemon.id}
+          pokemon={{}}
+          onAdd={() => {}}
+          isSelected={false}
+        />
+      ))}
+    </ListContainer>
+  );
+};
+
+export default PokemonList;
