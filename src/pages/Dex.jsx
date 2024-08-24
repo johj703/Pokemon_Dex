86e2faff629c64235fa90cf90e1485d380619f7a
Jo <johj703@nate.com>
2024-08-24 21:23:56 +0900

    Dex 페이지에 데이터 나오도록 작성

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import Dashboard from "../components/Dashboard";
+import PokemonList from "../components/PokemonList";
+import poketmonData from "../mock";
+
 const Dex = () => {
+  const [selectPokemonList, setSelectPokemonList] = useState([]);
   return (
     <div>
       <h1>포켓몬 도감</h1>
       <p>여기에 포켓몬 리스트들이 표시 됩니다!</p>
+      <Dashboard />
+      <PokemonList poketmonData={poketmonData} />
     </div>
   );
 };
