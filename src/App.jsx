8440d88dd88a38a25b01b1f48d35b2d970fbfd0c
Jo <johj703@nate.com>
2024-08-24 22:32:23 +0900

    App.css 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dex from "./pages/Dex";
 import PokemonDetail from "./components/PokemonDetail";
+import "./App.css";
 
 function App() {
   return (
