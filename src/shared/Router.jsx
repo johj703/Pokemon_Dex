45847ae48afbc754208c28675392bcf0b7053fa5
Jo <johj703@nate.com>
2024-08-26 16:35:56 +0900

    Router페이지에 Home, Dex, PokemonDetail 컴포넌트를 라우터로 등록

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Dex from "../pages/Dex";
+import PokemonDetail from "../pages/PokemonDetail";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/dex" element={<Dex />} />
+        <Route path="/detail" element={<PokemonDetail />} />
       </Routes>
     </BrowserRouter>
   );
