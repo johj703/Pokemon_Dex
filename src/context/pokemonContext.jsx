4a028f468449e85741d695a77e06c573d6ecd5ba
Jo <johj703@nate.com>
2024-08-26 21:58:44 +0900

    Dex 페이지에서 작성한 addPokemon, removePokemon을 pokemonContext로 이동

--- src/context/pokemonContext.jsx
+++ src/context/pokemonContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import MOCK_DATA from "../mock.js";
 
 export const PokemonContext = createContext(null);
 
@@ -9,26 +10,27 @@ export function usePokemon() {
 
 export function PokemonProvider({ children }) {
   const [selectedPokemonList, setSelectedPokemonList] = useState([]);
-
   const addPokemon = (pokemon) => {
-    selectedPokemonList(() => {
-      // 데이터는 각 고유 ID
-      if (selectedPokemonList.find((list) => list.id === pokemon.id)) {
-        alert("중복된 포켓몬이 있습니다.");
-        return [...selectedPokemonList];
-      }
-      return [...selectedPokemonList, pokemon];
-    });
-    console.log(selectedPokemonList);
-    if (selectedPokemonList.length > 6) {
+    if (selectedPokemonList.length > 5) {
       alert("최대 포켓몬은 6개 입니다.");
       return;
     }
 
-    selectedPokemonList((prev) => {
-      prev();
-    });
+    // 중복 선택시 alert 띄우는 함수
+    if (selectedPokemonList.find((list) => list.id === pokemon.id)) {
+      alert("중복된 포켓몬이 있습니다.");
+      return;
+    }
+
+    // 데이터는 각 고유 ID
+    if (MOCK_DATA.find((list) => list.id === pokemon.id)) {
+      setSelectedPokemonList([
+        ...selectedPokemonList,
+        MOCK_DATA.find((list) => list.id === pokemon.id),
+      ]);
+    }
   };
+
   const removePokemon = (pokemon) => {
     // 삭제할 때 상태값으로 저장된 배열을 다시 반환을 해야한다.
     // 삭제한 상태로 반환해야 한다!
@@ -36,6 +38,7 @@ export function PokemonProvider({ children }) {
       return selectedPokemonList.filter((list) => list.id !== pokemon.id);
     });
   };
+
   return (
     <PokemonContext.Provider
       value={{
