407373962dbfbca9d1a147016e2e308fcff10cce
Jo <johj703@nate.com>
2024-08-26 20:13:04 +0900

    PokemonContext를 useContext를 통해 가지고 와서 context 변수에 담기

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,8 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import PokemonCard from "./PokemonCard";
+import { useContext } from "react";
+import { PokemonContext } from "../context/pokemonContext";
 
 const PokemonList = ({ poketmonData, addPokemon }) => {
   const navigate = useNavigate();
+
+  const context = useContext(PokemonContext);
+  console.log(context);
+
   return (
     <>
       {poketmonData.map((pokemon) => {
