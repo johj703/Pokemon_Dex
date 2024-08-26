0a7b59520185a49d1df9f326bfacb30071a1fcd3
Jo <johj703@nate.com>
2024-08-26 16:35:24 +0900

    포켓몬 상세 페이지에서 item.id의 타입(number)과 pokemonId(string)의 타입이 다르기 때문에 pokemonId의 타입을 number로 변환하기 때문에 Number를 씌운다!

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -2,16 +2,19 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import MOCK_DATA from "../mock";
 
 function PokemonDetail() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const [searchParams] = useSearchParams();
   console.log(searchParams);
 
   const pokemonId = searchParams.get("id");
-
-  const pokemon = MOCK_DATA.find((item) => item.id === pokemonId);
-
   console.log(pokemonId);
+
+  const pokemon = MOCK_DATA.find((item) => item.id === Number(pokemonId));
+
+  console.log(pokemon);
+
+  const navigate = useNavigate();
 
   if (!pokemon) {
     return <div>포켓몬을 찾을 수 없습니다.</div>;
@@ -23,7 +26,13 @@ function PokemonDetail() {
       <h2>{pokemon.korean_name}</h2>
       <p>타입: {pokemon.types.join(", ")}</p>
       {/* onClick 이벤트를 완성해주세요. */}
-      <button onClick={() => {}}>뒤로 가기</button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 }
