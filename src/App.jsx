1c9b5a71e095cf029022e6b7d7a48ec818e18b3c
Jo <johj703@nate.com>
2024-08-26 16:03:59 +0900

    Router를 import해서 return 값에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dex from "./pages/Dex";
-import PokemonDetail from "./pages/PokemonDetail";
 import "./App.css";
+import Router from "./shared/Router";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dex" element={<Dex />} />
-        <Route path="/detail" element={<PokemonDetail />} />
-      </Routes>
-    </Router>
-  );
+  return <Router />;
 }
 
 export default App;
