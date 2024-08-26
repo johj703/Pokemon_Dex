75e64500dd06598c3e338b38ad683d9e33384a77
Jo <johj703@nate.com>
2024-08-26 17:45:20 +0900

    useContext 함수 작성

--- src/constext/pokemonContext.js
+++ src/constext/pokemonContext.js
@@ -1,3 +1,8 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const PokemonContext = createContext(null);
+
+// useContext 함수 작성
+export function usePokemon() {
+  return useContext(PokemonContext);
+}
