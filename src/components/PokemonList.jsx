18106b1bf594c270514b3f2dd189d940ad164f23
Jo <johj703@nate.com>
2024-08-26 23:35:25 +0900

    사용하지 않는 부분 삭제

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,13 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import PokemonCard from "./PokemonCard";
-import { useContext } from "react";
-import { PokemonContext } from "../context/pokemonContext";
 
 const PokemonList = ({ pokemonData }) => {
   const navigate = useNavigate();
 
-  // const context = useContext(PokemonContext);
-  // console.log(context);
   console.log(pokemonData);
   return (
     <>
