41b73b9d89bb398fb503eadd3bb45f309cd6be50
Jo <johj703@nate.com>
2024-08-26 16:33:38 +0900

    포켓몬 카드 리스트 컴포넌트에서는 추가 버튼을, Dex 컴포넌트에서는 삭제 버튼이 나타나도록 작성

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,31 @@
-function PokemonCard({ pokemon, addPokemon }) {
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Card = styled.div``;
+
+function PokemonCard({ pokemon, addPokemon, removePokemon, isSelect }) {
   console.log("pokemon => ", pokemon);
+  const navigate = useNavigate();
   return (
-    <div>
-      <img src={pokemon.img_url} />
-      <h2>{pokemon.korean_name}</h2>
-      <button type="button" onClick={() => addPokemon(pokemon)}>
-        추가
-      </button>
-    </div>
+    <Card>
+      <div
+        onClick={() => {
+          navigate(`/detail?id=${pokemon.id}`);
+        }}
+      >
+        <img src={pokemon.img_url} />
+        <h2>{pokemon.korean_name}</h2>
+      </div>
+      {isSelect ? (
+        <button type="button" onClick={() => removePokemon(pokemon)}>
+          삭제
+        </button>
+      ) : (
+        <button type="button" onClick={() => addPokemon(pokemon)}>
+          추가
+        </button>
+      )}
+    </Card>
   );
 }
 
