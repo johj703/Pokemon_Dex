fbcb780a04c3599e7b82258d179e438e77048681
Jo <johj703@nate.com>
2024-08-26 15:48:55 +0900

    App 페이지에서 옮겼던 디테일 페이지 주소로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dex from "./pages/Dex";
-import PokemonDetail from "./components/PokemonDetail";
+import PokemonDetail from "./pages/PokemonDetail";
 import "./App.css";
 
 function App() {
