356d15b85e7515401e81caab46f54cc64232daec
Jo <johj703@nate.com>
2024-08-27 10:54:07 +0900

    아래쪽에 있는 pokemonList들 가로 정렬 및 컨테이너에 최대한 붙여서 정렬되도록 작성

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,12 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import PokemonCard from "./PokemonCard";
+import styled from "styled-components";
+
+const PokemonListContainer = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  gap: 10px;
+  width: 1200px;
+  justify-content: space-between;
+  margin: auto;
+`;
 
 const PokemonList = ({ pokemonData }) => {
   const navigate = useNavigate();
 
   console.log(pokemonData);
   return (
-    <>
+    <PokemonListContainer>
       {pokemonData.map((pokemon) => {
         return (
           <PokemonCard
@@ -19,7 +29,7 @@ const PokemonList = ({ pokemonData }) => {
           />
         );
       })}
-    </>
+    </PokemonListContainer>
   );
 };
 
