11a8e35e21fc69de4601fe964045cc2ffaff4d4e
Jo <johj703@nate.com>
2024-08-27 10:51:49 +0900

    PokemonList에서 선택된 Dashboard의 포켓몬들 가로정렬, gap 추가가 되도록 작성

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,19 +1,26 @@
 import { useContext } from "react";
 import PokemonCard from "./PokemonCard";
 import { PokemonContext } from "../context/pokemonContext";
+import styled from "styled-components";
+
+const DashBoardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 100px;
+`;
 
 const Dashboard = () => {
   const { selectedPokemonList } = useContext(PokemonContext);
 
   console.log(selectedPokemonList);
   return (
-    <>
+    <DashBoardContainer>
       {selectedPokemonList.map((pokemon) => {
         return (
           <PokemonCard key={pokemon.id} pokemon={pokemon} isSelect={true} />
         );
       })}
-    </>
+    </DashBoardContainer>
   );
 };
 
